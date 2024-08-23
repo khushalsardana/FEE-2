fbddb4236d0b398eff136ec080c3d2a7466111d2
khushalsardana <khushalsardana926@gmail.com>
2024-08-23 15:17:12 +0530

    Lecture 20

---
+++ Classwork/data_type.js
@@ -0,0 +1,25 @@
+// number data type
+let a = 10;
+let b = -20;
+let c = 2.5;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+
+//string data type
+let name = "John";
+console.log(typeof(name))
+
+//boolean
+let x = true;
+console.log(typeof(x))
+
+//undefined
+let j;
+console.log(typeof(j))
+j=20;
+console.log(typeof(j))
+
+//null 
+let k = null;
+console.log(typeof(k))  //its data type would be object not null
