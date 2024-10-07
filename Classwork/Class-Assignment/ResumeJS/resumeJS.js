b9a1c8ffd7c2bec08b0e241864734e378a1fadea
khushalsardana <khushalsardana926@gmail.com>
2024-10-07 11:32:49 +0530

    Resume Using JS

---
+++ Classwork/Class-Assignment/ResumeJS/resumeJS.js
@@ -0,0 +1,35 @@
+let a = document.querySelector("#heading1");
+a.style.color = "yellowgreen"; 
+
+
+let b = document.querySelector("#para1");
+b.style.color = "yellowgreen";
+
+
+let c = document.querySelector("#head1")
+c.style.color = "whitesmoke";
+
+
+let d = document.querySelector("#head2")
+d.style.color = "whitesmoke";
+
+
+let e = document.getElementById("head3")
+e.style.color = "whitesmoke";
+
+
+let f = document.querySelector("#head4")
+f.style.color = "whitesmoke";
+
+
+let g = document.getElementsByClassName("cont");
+g[0].style.color = "rgb(208, 188, 188)" 
+g[1].style.color = "rgb(208, 188, 188)" 
+g[2].style.color = "rgb(208, 188, 188)" 
+g[3].style.color = "rgb(208, 188, 188)" 
+g[4].style.color = "rgb(208, 188, 188)" 
+
+
+
+let h = document.querySelector("#paragraph");
+h.style.color="aqua";
