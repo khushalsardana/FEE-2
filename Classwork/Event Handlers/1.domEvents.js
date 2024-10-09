7828a1aac005c24217e5e9d65306cf929512ddc7
khushalsardana <khushalsardana926@gmail.com>
2024-10-09 14:46:00 +0530

    Lecture 33

---
+++ Classwork/Event Handlers/1.domEvents.js
@@ -0,0 +1,73 @@
+// // 1.
+// var btn = document.querySelector("button");
+// console.dir(btn);
+
+// btn.onclick = function () {
+//   console.log("button pressed");
+// };
+
+
+// // 2.
+// var btns = document.querySelectorAll("button");
+
+// for (btn of btns) {
+//   btn.onclick = myfunc;
+//   btn.onmouseenter = function cursor() {
+//     console.log("mouse is over the button");
+//   };
+// }
+// function myfunc() {
+//   console.log("button pressed");
+// }
+
+
+
+// // 3.
+// var btns = document.querySelectorAll("button");
+
+// for (btn of btns) {
+//   btn.onclick = myfunc;
+//   btn.onclick = hello;
+// }
+// function myfunc() {
+//   console.log("button pressed");
+// }
+// function hello() {
+//   console.log("hello");
+// }
+
+
+
+
+//4. event listener:
+// var btns = document.querySelectorAll("button");
+
+// for (btn of btns) {
+//   //   btn.onclick = myfunc;
+//   //   btn.onclick = hello;
+//   btn.addEventListener("click", myfunc);
+//   btn.addEventListener("click", hello);
+// }
+// function myfunc() {
+//   console.log("button pressed");
+// }
+// function hello() {
+//   console.log("hello");
+// }
+
+
+
+
+// 5.event listener on elements:
+var p = document.querySelector("p");
+
+p.addEventListener("mouseenter", function myfunc() {
+  console.log("mouse was over me ");
+});
+
+
+
+
+
+
+
