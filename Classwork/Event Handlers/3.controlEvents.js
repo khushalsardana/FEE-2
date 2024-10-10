cce138e0443400e690cf9cd6ebc76b1fd40c5961
khushalsardana <khushalsardana926@gmail.com>
2024-10-10 11:05:20 +0530

    Lecture 33

---
+++ Classwork/Event Handlers/3.controlEvents.js
@@ -0,0 +1,20 @@
+// // 1. keyup:
+// inp.addEventListener("keyup", function () {
+//   console.log("Key was Released");
+// });
+
+
+// //2. keydown with event value of code and key value
+// inp.addEventListener("keydown", function (e) {
+//   console.log(e);
+//   console.log("Key was Pressed");
+// });
+
+
+
+let inp = document.querySelector("input");
+inp.addEventListener("keydown", function (e) {
+  console.log("key:" + e.code);
+  console.log("value" + e.key);
+});
+
